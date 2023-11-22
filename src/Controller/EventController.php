bc01be6b6160d2103af79ffa10035055a4c80235
Mirek Nguyen <mireknguyenbinh@seznam.cz>
2023-11-22 12:51:40 +0100

    feat: flash when adding event

--- src/Controller/EventController.php
+++ src/Controller/EventController.php
@@ -6,16 +6,28 @@ use App\Entity\Event;
 use App\Form\EventType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class EventController extends AbstractController
 {
     #[Route('/', name: 'app_index')]
-    public function index(): Response
+    public function index(Request $request, EntityManagerInterface $entityManager): Response
     {
         $event = new Event();
         $form = $this->createForm(EventType::class, $event);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            // $form->getData() holds the submitted values
+            // but, the original `$task` variable has also been updated
+            $event = $form->getData();
+            $entityManager->persist($event);
+            $entityManager->flush();
+
+            $this->addFlash('success', 'Event added ');
+        }
+
         return $this->render('event/index.html.twig', [
             'form' => $form,
         ]);
