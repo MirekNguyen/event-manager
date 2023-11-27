986d3aef71b13e89d03fd7a70088ac91d66279f4
Mirek Nguyen <mireknguyenbinh@seznam.cz>
2023-11-27 01:02:22 +0100

    fix: datetime picker 24h format

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,35 +1,19 @@
-const $ = require('jquery');
 require('@popperjs/core');
 require('@fortawesome/fontawesome-free/js/all');
 const tempusdominus = require('@eonasdan/tempus-dominus');
 
-const picker = new tempusdominus
+const startDatePicker = new tempusdominus
   .TempusDominus(document.getElementById('event_start_date'), {
     localization: {
       format: 'dd/MM/yyyy HH:mm',
+      locale: 'cs-GB',
   }
   });
-const picker2 = new tempusdominus
+const endDatePicker = new tempusdominus
   .TempusDominus(document.getElementById('event_end_date'), {
     localization: {
       format: 'dd/MM/yyyy HH:mm',
+      locale: 'en-GB',
   }
   });
-
-const event_filter_start = new tempusdominus
-  .TempusDominus(document.getElementById('event_filter_start_date'), {
-    localization: {
-      format: 'dd/MM/yyyy HH:mm',
-  }
-  });
-const event_filter_end = new tempusdominus
-  .TempusDominus(document.getElementById('event_filter_end_date'), {
-    localization: {
-      format: 'dd/MM/yyyy HH:mm',
-  }
-  });
-
-
-$(document).ready(function() {
-});
 
